916db45aae7aa221f6e98796e45346573e4f55ee
thenameisakashp <myownworld1428@gmail.com>
2026-05-10 08:01:26 +0530

    adding character hashing and recursion

--- hashing.py
+++ hashing.py
@@ -7,7 +7,7 @@ for i in m:
             count+=1
     print(count)'''
 
-n =[5,3,2,2,1,5,5,7,5,10]
+'''n =[5,3,2,2,1,5,5,7,5,10]     #hashing optimal way to count the frequency of elements in a list
 m = [10,111,1,9,5,67,2]
 hash_list = [0]*11
 for num in n:
@@ -15,6 +15,17 @@ for num in n:
 for number in m:
     if number<1 or number>10:
         print(0)
-
     else:
-        print(hash_list[number])
+        print(hash_list[number])'''
+
+'''n =[5,3,2,2,1,5,5,7,5,10]     #hashing optimal way to count the frequency of elements in a list using dictionary
+m = [10,111,1,9,5,67,2]
+num = len(n)
+hash_list = dict()  
+for i in range(0,num):
+    hash_list[n[i]] = hash_list.get(n[i],0)+1
+for number in m:
+    if number<1 or number>10:
+        print(0)
+    else:
+        print(hash_list.get(number,0))'''
