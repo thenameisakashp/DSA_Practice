5c5c769fac58f9b0ea4f850957e93ee67d172413
thenameisakashp <myownworld1428@gmail.com>
2026-05-11 21:41:04 +0530

    adding different problems

--- recursion_parameter.py
+++ recursion_parameter.py
@@ -1,6 +1,36 @@
-'''def func(x,N):
+'''def func(x,N):  #recursion using parameter
     if N == 0:
         return
     print(x)
     func(x,N-1)
 func(15,4)'''
+
+'''def func(i,N):  # head recursion using parameter to print 1 to N
+    if i > N:
+        return
+    print(i)
+    func(i+1,N)
+func(1,4)'''
+
+'''def func(i,N):  # tail recursion using parameter to print N to 1
+    if i > N:
+        return
+    func(i+1,N)
+    print(i)
+func(1,4)'''
+
+'''def func(N):  # head recursion using parameter to print N to 1
+    if N == 0:
+        return
+    print(N)
+    func(N-1)
+func(4)'''
+
+
+'''def func(N):  # tail recursion using parameter to print 1 to N
+    if N == 0:
+        return
+    func(N-1)
+    print(N)
+
+func(4)'''
